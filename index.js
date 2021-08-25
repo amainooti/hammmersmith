1ae4c3316bc23abb25d3fc58c47a461dd3c7412e
Abu David <davidabu80@gmail.com>
2021-08-25 20:05:06 +0100

    Slider

--- index.js
+++ index.js
@@ -18,3 +18,16 @@ const navSlide = () => {
   });
 };
 navSlide();
+
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  effect: "fade",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
